291ea30cdcd62fa60c7e77029f09c00cc33fa30e
krommavutn <107599697+krommavutn@users.noreply.github.com>
2022-06-16 11:19:13 +0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,4 +5,13 @@ if n == 10:
     print('n equal to 10')
 else:
     print('n is something else except 10')
+    print('kromma edite')
+    n = int(input())
+    total = 0
+    i = 1
+        while i <= n:
+            total = total + i
+            i = i + 1
+        print(total)
+
 
