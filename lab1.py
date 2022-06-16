4c95cd1655ec01382ad946d17ed8eac2a845b20e
kromma <krommavut.n@gmail.com>
2022-06-16 11:22:38 +0700

    new commit

--- lab1.py
+++ lab1.py
@@ -1,14 +1,12 @@
 print('Hello RmutSB Sci CS ITS 16541N')
 #//////////////////////////
-n = 5
-if n == 10:
+m = 5
+if m == 10:
     print('n equal to 10')
 else:
     print('n is something else except 10')
     print('kromma edite')
 count = 1
 while count <= 8:
-     print('I like Bossanova')
+     print('I like kromma')
      count = count + 1
-
-
