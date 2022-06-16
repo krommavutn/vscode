64d41037f057b243f9f1eda6a0304fbeca03bcf3
kromma <krommavut.n@gmail.com>
2022-06-16 11:17:32 +0700

    new commit

--- lab1.py
+++ lab1.py
@@ -5,20 +5,4 @@ if n == 10:
     print('n equal to 10')
 else:
     print('n is something else except 10')
-#////////////////////////////
-level = input('Enter level (1 - 4): ')
-if level == '1':
-    print('Easy')
-elif level == '2':
-    print('Medium')
-elif level == '3':
-    print('Hard')
-elif level == '4':
-    print('Expert')
-else:
-    print('Invalid level selected')
-#////////////////////////////////
-count = 1
-while count <= 8:
-     print('I like RmutSB Sci')
-     count = count + 1
+
