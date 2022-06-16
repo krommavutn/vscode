e6b82d6025b7da61c2af2bb37364da639b6d6802
krommavutn <krommavut.n@rmutsb.ac.th>
2022-06-16 10:50:10 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,19 @@
+print('Hello RmutSB Sci CS ITS 16541N')
+#//////////////////////////
+n = 5
+if n == 10:
+    print('n equal to 10')
+else:
+    print('n is something else except 10')
+#////////////////////////////
+level = input('Enter level (1 - 4): ')
+if level == '1':
+    print('Easy')
+elif level == '2':
+    print('Medium')
+elif level == '3':
+    print('Hard')
+elif level == '4':
+    print('Expert')
+else:
+    print('Invalid level selected')
